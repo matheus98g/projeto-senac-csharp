647962fd5500ab9b51a20a69c8f086de4b366acd
matheus98g <gollmannmatheus@gmail.com>
2025-08-07 20:01:25 -0300

    [frontend] add dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "@/components/dark-mode/theme-provider"
+import { ModeToggle } from "@/components/dark-mode/theme-togle"
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button className="cursor-pointer">Click me</Button>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button className="cursor-pointer">Click me</Button>
+        <ModeToggle />
+      </div>
+    </ThemeProvider>
   )
 }
 
