94f34c476d85c18ffe79326c008ca048bf6530c4
Samuel-Goulart <188399307+Leonardo-Henrique-Gabbi@users.noreply.github.com>
2025-08-27 20:01:45 -0300

    Update ProfessorService.cs

--- backend/PortalEducaAPI/PortalEducaAPI.Domain/Service/ProfessorService.cs
+++ backend/PortalEducaAPI/PortalEducaAPI.Domain/Service/ProfessorService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using PortalEducaAPI.Domain.Dtos.Request.Professor;
+﻿using PortalEducaAPI.Domain.Dtos.Request.Professor;
 using PortalEducaAPI.Domain.Dtos.Response.Professor;
 using PortalEducaAPI.Domain.Models;
 using PortalEducaAPI.Domain.Repository;
